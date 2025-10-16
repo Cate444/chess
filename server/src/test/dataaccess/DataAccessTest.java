3252719aeb900733b935988d1306f56ee4e57af0
Cate Allen <cateallen@MacBook-Pro-188.app.byu.edu>
2025-10-16 10:33:26 -0600

    clear and createUser test

---
+++ server/src/test/dataaccess/DataAccessTest.java
@@ -0,0 +1,33 @@
+package dataaccess;
+
+import datamodel.UserData;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataAccessTest {
+
+    @Test
+    void clear() {
+        DataAccess db = new MemoryDataAccess();
+        db.createUser(new UserData("joe", "j@j.com", "passThisWord"));
+        db.clear();
+        assertNull(db.getUser("joe"));
+    }
+
+    @Test
+    void createUser() {
+        DataAccess db = new MemoryDataAccess();
+        var user = new UserData("joe", "j@j.com", "passThisWord");
+        db.createUser(user);
+        assertEquals(user, db.getUser(user.username()));
+    }
+
+    @Test
+    void getUser() {
+    }
+
+    @Test
+    void getAuthToken() {
+    }
+}
