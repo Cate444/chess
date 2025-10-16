35164f8f34ba462d40919e775bb60d577ae87114
Cate Allen <cateallen@MacBook-Pro-188.app.byu.edu>
2025-10-16 10:44:29 -0600

    register test written

---
+++ server/src/test/service/UserServiceTest.java
@@ -0,0 +1,31 @@
+package service;
+
+import dataaccess.DataAccess;
+import dataaccess.MemoryDataAccess;
+import datamodel.UserData;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+   @Test
+   void clear(){
+
+   }
+
+    @Test
+    void register() throws Exception{
+        DataAccess db = new MemoryDataAccess();
+        UserService service = new UserService(db);
+        var user = new UserData("joe", "j@j.com", "passThisWord");
+        var authData = service.register(user);
+        assertNotNull(authData);
+        assertEquals(user.username(), authData.username());
+        assertTrue(!authData.authToken().isEmpty());
+    }
+
+    @Test
+    void login() {
+    }
+}
