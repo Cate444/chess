a4b9449fc281f95c5e768f77a3eb3b13da65d042
Cate Allen <cateallen@MacBook-Pro-254.app.byu.edu>
2025-10-29 12:04:21 -0600

    game connection is done

--- server/src/main/java/dataaccess/SQLGameDataAccess.java
+++ server/src/main/java/dataaccess/SQLGameDataAccess.java
@@ -4,9 +4,35 @@ import datamodel.GameName;
 import datamodel.JoinInfo;
 import datamodel.ReturnGameData;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class SQLGameDataAccess implements GameDataAccess {
+
+    public SQLGameDataAccess() throws Exception{
+        try (Connection conn = DatabaseManager.getConnection()) {
+            for (String statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new Exception(String.format("Unable to configure database: %s", ex.getMessage()));
+        }
+    }
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  gamesTable (
+              `gameID` int NOT NULL AUTO_INCREMENT,
+              `whiteUsername` varchar(256) NOT NULL,
+              `blackUsername` varchar(256) NOT NULL,
+              `game` TEXT NOT NULL,
+               PRIMARY KEY (`gameID`)
+            )
+            """
+    };
 
     @Override
     public void clear() {
