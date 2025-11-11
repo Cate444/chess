b5c784a8a5788c23ea49cc5557179f45060770d2
Cate Allen <cateallen@MacBook-Pro-341.local>
2025-11-11 15:07:12 -0700

    try again

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -3,13 +3,11 @@ package client;
 import chess.ChessGame;
 import datamodel.AuthData;
 import datamodel.ReturnGameData;
-import datamodel.UserData;
 import org.junit.jupiter.api.*;
 import server.Server;
 import server.ServerFacade;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,7 +16,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ServerFacadeTests {
 
     private static Server server;
-    private static final ServerFacade serverFacade = new ServerFacade("http://localhost:8080");
+    private static final ServerFacade SERVER_FACADE = new ServerFacade("http://localhost:8080");
 
     @BeforeAll
     public static void init() {
@@ -40,105 +38,105 @@ public class ServerFacadeTests {
 
     @Test
     public void clear() {
-        assertDoesNotThrow(serverFacade::clear);
+        assertDoesNotThrow(SERVER_FACADE::clear);
     }
 
     @Test
     public void clearBad() throws Exception{
-        AuthData userData = serverFacade.register("u", "e", "p");
-        serverFacade.logout(userData.authToken());
-        serverFacade.login("u", "p");
-        assertDoesNotThrow(serverFacade::clear);
+        AuthData userData = SERVER_FACADE.register("u", "e", "p");
+        SERVER_FACADE.logout(userData.authToken());
+        SERVER_FACADE.login("u", "p");
+        assertDoesNotThrow(SERVER_FACADE::clear);
     }
 
     @Test
     public void register() throws Exception{
-        serverFacade.clear();
-        AuthData userData = serverFacade.register("u", "e", "p");
+        SERVER_FACADE.clear();
+        AuthData userData = SERVER_FACADE.register("u", "e", "p");
         assertEquals( "u", userData.username());
     }
 
     @Test
     public void registerAgain() throws Exception{
-        serverFacade.clear();
-        serverFacade.register("Tyler", "the", "best");
-        assertThrows(java.lang.Exception.class, ()-> serverFacade.register("Tyler", "the", "best"));
+        SERVER_FACADE.clear();
+        SERVER_FACADE.register("Tyler", "the", "best");
+        assertThrows(java.lang.Exception.class, ()-> SERVER_FACADE.register("Tyler", "the", "best"));
     }
 
     @Test
     public void logout() throws Exception{
-        serverFacade.clear();
-        AuthData userData = serverFacade.register("Tyler", "the", "best");
-        assertDoesNotThrow(()->serverFacade.logout(userData.authToken()));
+        SERVER_FACADE.clear();
+        AuthData userData = SERVER_FACADE.register("Tyler", "the", "best");
+        assertDoesNotThrow(()-> SERVER_FACADE.logout(userData.authToken()));
     }
 
     @Test
     public void logoutWrongAuthToken() throws Exception{
-        serverFacade.clear();
-        AuthData userData = serverFacade.register("Tyler", "the", "best");
-        assertThrows(java.lang.Exception.class, ()->serverFacade.logout(userData.username()));
+        SERVER_FACADE.clear();
+        AuthData userData = SERVER_FACADE.register("Tyler", "the", "best");
+        assertThrows(java.lang.Exception.class, ()-> SERVER_FACADE.logout(userData.username()));
     }
 
     @Test
     public void login() throws Exception{
-        serverFacade.clear();
-        AuthData userData = serverFacade.register("Tyler", "the", "best");
-        serverFacade.logout(userData.authToken());
-        assertDoesNotThrow(()->serverFacade.login("Tyler", "best"));
+        SERVER_FACADE.clear();
+        AuthData userData = SERVER_FACADE.register("Tyler", "the", "best");
+        SERVER_FACADE.logout(userData.authToken());
+        assertDoesNotThrow(()-> SERVER_FACADE.login("Tyler", "best"));
     }
 
     @Test
     public void loginNonexistentUser() throws Exception{
-        serverFacade.clear();
-        assertThrows(java.lang.Exception.class,()->serverFacade.login("Tyler", "best"));
+        SERVER_FACADE.clear();
+        assertThrows(java.lang.Exception.class,()-> SERVER_FACADE.login("Tyler", "best"));
     }
 
     @Test
     public void createGame() throws Exception{
-        serverFacade.clear();
-        AuthData userData = serverFacade.register("Tyler", "the", "best");
-        assertDoesNotThrow(()-> serverFacade.createGame(userData.authToken(), "Agame"));
+        SERVER_FACADE.clear();
+        AuthData userData = SERVER_FACADE.register("Tyler", "the", "best");
+        assertDoesNotThrow(()-> SERVER_FACADE.createGame(userData.authToken(), "Agame"));
     }
 
     @Test
     public void createBadGame() throws Exception{
-        serverFacade.clear();
-        assertThrows(Exception.class,()-> serverFacade.createGame("athToken", "Agame"));
+        SERVER_FACADE.clear();
+        assertThrows(Exception.class,()-> SERVER_FACADE.createGame("athToken", "Agame"));
     }
 
     @Test
     public void listGame() throws Exception{
-        serverFacade.clear();
-        AuthData userData = serverFacade.register("Tyler", "the", "best");
-        Map<String, Object> gameList = serverFacade.listGames(userData.authToken());
+        SERVER_FACADE.clear();
+        AuthData userData = SERVER_FACADE.register("Tyler", "the", "best");
+        Map<String, Object> gameList = SERVER_FACADE.listGames(userData.authToken());
         assertEquals(1, gameList.size());
     }
 
     @Test
     public void listGamesUnauthorized() throws Exception{
-        serverFacade.clear();
-        assertThrows(Exception.class ,()->serverFacade.listGames("token"));
+        SERVER_FACADE.clear();
+        assertThrows(Exception.class ,()-> SERVER_FACADE.listGames("token"));
     }
 
     @Test
     public void joinGame() throws Exception{
-        serverFacade.clear();
-        AuthData userData = serverFacade.register("Tyler", "the", "best");
-        serverFacade.createGame(userData.authToken(), "Agame");
-        Map<String, Object> gameList = serverFacade.listGames(userData.authToken());
+        SERVER_FACADE.clear();
+        AuthData userData = SERVER_FACADE.register("Tyler", "the", "best");
+        SERVER_FACADE.createGame(userData.authToken(), "Agame");
+        Map<String, Object> gameList = SERVER_FACADE.listGames(userData.authToken());
         ArrayList<ReturnGameData> gameDatas = (ArrayList<ReturnGameData>) gameList.get("games");
         ReturnGameData gameData = gameDatas.get(0);
-        assertDoesNotThrow(()->serverFacade.joinGame(userData.authToken(), gameData.gameID(), ChessGame.TeamColor.WHITE));
+        assertDoesNotThrow(()-> SERVER_FACADE.joinGame(userData.authToken(), gameData.gameID(), ChessGame.TeamColor.WHITE));
     }
 
     @Test
     public void joinGamesUnauthorized() throws Exception{
-        serverFacade.clear();
-        AuthData userData = serverFacade.register("Tyler", "the", "best");
-        serverFacade.createGame(userData.authToken(), "Agame");
-        Map<String, Object> gameList = serverFacade.listGames(userData.authToken());
+        SERVER_FACADE.clear();
+        AuthData userData = SERVER_FACADE.register("Tyler", "the", "best");
+        SERVER_FACADE.createGame(userData.authToken(), "Agame");
+        Map<String, Object> gameList = SERVER_FACADE.listGames(userData.authToken());
         ArrayList<ReturnGameData> gameDatas = (ArrayList<ReturnGameData>) gameList.get("games");
         ReturnGameData gameData = gameDatas.get(0);
-        assertThrows(Exception.class ,()->serverFacade.joinGame("token", gameData.gameID(), ChessGame.TeamColor.WHITE));
+        assertThrows(Exception.class ,()-> SERVER_FACADE.joinGame("token", gameData.gameID(), ChessGame.TeamColor.WHITE));
     }
 }
